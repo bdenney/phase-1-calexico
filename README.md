33ce230989db35a42444c80929327e6291a91055
John Finley <johnfinleybremerton@gmail.com>
2022-07-06 11:56:15 -0600

    Finished lab

--- README.md
+++ README.md
@@ -24,15 +24,19 @@ Then, open the `index.html` file on your browser to run the application.
 
 Write your code in the `index.js` file.
 
+//////
 ## Challenge #1
 Fetch all the menu items from `http://localhost:3000/menu`. For each menu item create a `span` element that contains the name of the menu item, and add it to the `#menu-items` div.
 
+/////
 ## Challenge #2
 When the page loads, display the first menu item. You should set the image, name, description, and price. All the correct elements to set are located in the `#dish` `section` element.
 
+////////
 ## Challenge #3
 When the user clicks on the menu items on the left, they should see all the details for that specific menu item.
 
+//////
 ## Challenge #4
 The user should be able to add the menu items to their cart. When the user presses the 'Add to Cart' button, that number should be added to however many are currently in the cart.
 
@@ -42,11 +46,14 @@ For example, if there are currently 2 burritos in the cart, and the user adds 2 
 ---
 # Bonus Challenges!
 
+////////////
 ## Bonus Challenge #1
 The value of the cart for each item should persist when clicking through the various menu items.
 
+/////////
 ## Bonus Challenge #2 
 Calculate the *total cost* of what is currently in the cart and display it somewhere on the page.
 
+///////////
 ## Bonus Challenge #3
 Use PATCH requests to maintain the value of the cart in-between refreshes.
--- index.js
+++ index.js
@@ -1,1 +1,79 @@
 // Write your code here...
+const menuURL = "http://localhost:3000/menu"
+
+document.addEventListener('DOMContentLoaded', () => {
+    initialize()
+})
+
+function initialize() {
+    fetch(menuURL)
+        .then(res => res.json())
+        .then(foodItems => {
+            renderMenuItems(foodItems)
+        })
+    pageLoad()
+    submitForm()
+}
+
+function renderMenuItems(foodItems) {
+    foodItems.forEach(foodItem => {
+        const menuItems = document.querySelector("#menu-items");
+        const span = document.createElement("span")
+        span.textContent = foodItem.name
+        menuItems.appendChild(span)
+        span.setAttribute('identification', `${foodItem.id}`)
+        span.addEventListener('click', () => {
+            let attribute = span.getAttribute("identification");
+            pageLoad(attribute)
+        })
+    })
+}
+
+function pageLoad(attribute = 1) {
+    fetch(`${menuURL}/${attribute}`)
+        .then(res => res.json())
+        .then((foodItem) => {
+            const dishImage = document.querySelector("#dish-image")
+            const dishName = document.querySelector("#dish-name")
+            const dishDescription = document.querySelector("#dish-description")
+            const dishPrice = document.querySelector("#dish-price")
+            const cartNumber = document.querySelector("#number-in-cart")
+            const cartPrice = document.querySelector("#price-of-cart")
+            dishImage.src = foodItem.image
+            dishName.textContent = foodItem.name
+            dishName.setAttribute("identification", attribute)
+            dishDescription.textContent = foodItem.description
+            dishPrice.textContent = `${foodItem.price}$`
+            cartNumber.textContent = Number(foodItem.number_in_bag);
+            const span = document.querySelector("#number-in-cart")
+            cartPrice.textContent = `Your total price is ${Number(dishPrice.textContent.slice(0, -1)) * Number(span.textContent)}$`
+        })
+}
+
+function submitForm() {
+    const form = document.querySelector("#cart-form")
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        let span = document.querySelector("#number-in-cart")
+        span.textContent = Number(span.textContent) + Number(form["cart-amount"].value)
+        const dishName = document.querySelector("#dish-name")
+        const dishPrice = document.querySelector("#dish-price")
+        const cartPrice = document.querySelector("#price-of-cart")
+        let attribute = dishName.getAttribute("identification");
+        fetch(`${menuURL}/${attribute}`, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                number_in_bag: Number(span.textContent),
+            }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(() => {
+            form.reset()
+            cartPrice.textContent = `Your total price is ${Number(dishPrice.textContent.slice(0, -1)) * Number(span.textContent)}$`
+        })
+        
+    })
+}
